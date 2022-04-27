00d5755ea88cf4e8f329f516d1a650c37d3f1fe5
SDuncan5 <sduncan1229@gmail.com>
2022-04-27 08:42:53 -0700

    cont commit

---
+++ src/Interfaces/Interfaces.java
@@ -0,0 +1,25 @@
+package Interfaces;
+
+public class Interfaces {
+
+    public static void main(String[] args) {
+
+        // Interface = a template that can be applied to a class
+        //             similar to inheritance, but specifies what a class has/must do
+        //             classes can apply more than one interface, inheritance is limited to 1 super
+
+        Rabbit rabbit = new Rabbit();
+        Hawk hawk = new Hawk();
+        Fish fish = new Fish();
+
+        rabbit.flee();
+
+        hawk.hunt();
+
+        fish.hunt();
+        fish.flee();
+
+
+    }
+
+}
